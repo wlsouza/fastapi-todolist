7deede2752b60f86b815ea62375842351d1ca87b
Wellington Lorenço de Souza <wlsouza@protonmail.com>
2021-09-04 12:26:44 -0300

    add external_id in task model and schema

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,8 @@
+from uuid import uuid4
+
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 from app.database.base_class import Base
 
@@ -7,6 +10,7 @@ from app.database.base_class import Base
 class Task(Base):
 
     id = Column(Integer, primary_key=True, index=True)
+    external_id = Column(UUID(as_uuid=True), index=True, unique=True, default=uuid4)
     title = Column(String, index=True)
     description = Column(String, index=True)
     is_done = Column(Boolean)
--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,15 +1,10 @@
-from pydantic import BaseModel, constr, UUID4
-from enum import Enum
-
-class TaskStates(str, Enum):
-    done = "done"
-    not_done = "not-done"
+from pydantic import BaseModel, Field, UUID4
 
 # Shared properties
 class TaskBase(BaseModel):
-    title: constr(min_length=3, max_length=50)
-    description: constr(max_length=140)
-    state: TaskStates = TaskStates.not_done
+    title: str = Field(min_length=3, max_length=50)
+    description: str = Field(max_length=140)
+    is_done: bool
 
 # Properties to receive on item creation
 class TaskCreate(TaskBase):
@@ -19,8 +14,24 @@ class TaskCreate(TaskBase):
 class TaskUpdate(TaskBase):
     pass
 
-# Properties to return to client
-class Task(TaskBase):
-    id: UUID4
+# Properties shared by models stored in DB
+class TaskInDBBase():
+    external_id: UUID4
+    title: str
+    description: str
+    is_done: bool
+    owner_id: int
 
+    class Config:
+        orm_mode = True
+
+# Properties to return to client
+class Task(TaskInDBBase):
+    external_id: UUID4 = Field(alias="id")
+    is_done: bool = Field(alias="done")
+
+
+# Properties properties stored in DB
+class TaskInDB(TaskInDBBase):
+    pass
 
