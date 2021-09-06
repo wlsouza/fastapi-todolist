92b01db33e51564bf88b9bbd7ef0e5f417d0cd20
Wellington Lorenço de Souza <wlsouza@protonmail.com>
2021-09-06 11:28:43 -0300

    add first alembic revision

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,11 @@
+from typing import Union
+
 from pydantic import BaseSettings, PostgresDsn
+
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SQLALCHEMY_DATABASE_URI: PostgresDsn = "postgresql://localhost:5432/todolist_db"
+    SQLALCHEMY_DATABASE_URI: Union[str, PostgresDsn] = 'postgresql://postgres:postgres@localhost:5432/foobar'
     
     class Config:
         env_file = ".env"
--- app/database/base.py
+++ app/database/base.py
@@ -1,3 +1,5 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.database.base_class import Base  # noqa
+from app.models.task import Task  # noqa
+from app.models.user import User  # noqa
