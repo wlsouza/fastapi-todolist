fa141b88d07f487b82f74c4215b73db0c833004a
Wellington Lorenço <wlsouza@protonmail.com>
2021-09-18 23:54:38 -0300

    add schemas of user

---
+++ app/schemas/user.py
@@ -0,0 +1,39 @@
+from app.models.user import User
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, SecretStr
+
+
+# Shared properties
+class UserBase(BaseModel):
+    full_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = True
+    is_superuser: bool = False
+    
+
+# Properties to receive via API on creation    
+class UserCreate(UserBase):
+    full_name: str
+    email: EmailStr
+    password: SecretStr
+
+
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    password: Optional[SecretStr] = None
+
+# Properties shared by models stored in DB
+class UserInDBBase(UserBase):
+    id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+# Properties to return to client
+class User(UserInDBBase):
+    pass
+
+# Properties properties stored in DB
+class UserInDB(UserInDBBase):
+    hashed_password: str
