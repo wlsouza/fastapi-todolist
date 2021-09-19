49484ce9cb6becfddad8f860576e7d88f54435c0
Wellington Lorenço <wlsouza@protonmail.com>
2021-09-18 23:15:59 -0300

    fix: fix test name of login test

--- app/tests/api/api_v1/test_login.py
+++ app/tests/api/api_v1/test_login.py
@@ -1,11 +1,11 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-from requests.sessions import Session
+from sqlalchemy.orm import Session
 
 from app.core.config import settings
 from app import schemas
 
-def test_resource_token_must_accept_port_verb(client: TestClient) -> None:
+def test_resource_token_must_accept_post_verb(client: TestClient) -> None:
     response = client.post(f"{settings.API_V1_STR}/login/access-token")
     assert response.status_code != status.HTTP_405_METHOD_NOT_ALLOWED
 
