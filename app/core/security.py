7477d1915b26b1d45a5719a9f249abcaabbb7d90
Wellington Lorenço <wlsouza@protonmail.com>
2021-12-10 19:08:28 -0300

    feat: add methods to manage jwt tokens

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,38 @@
-from passlib.context import CryptContext
+from typing import Union
+from datetime import datetime, timedelta
 
+import asyncio
+from passlib.context import CryptContext
+from jose import jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app import crud
+from app.models import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password:str) -> str:
     return pwd_context.hash(password)
+
+def verify_password(raw_password:str, hashed_password:str) -> bool:
+    return pwd_context.verify(raw_password, hashed_password)
+
+
+def create_access_token(subject:str, expires_delta:timedelta= None) -> str:
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=2880) #TODO: Insert minutes in config file
+    payload = {"exp": expire, "sub": subject}
+    encoded_jwt = jwt.encode(payload, "secret", algorithm="HS256") #TODO: insert secret in config file 
+    return encoded_jwt
+
+
+# TODO: Think if authenticate_user method should be in CrudUser class 
+def authenticate_user(db:AsyncSession, user_id:int , password:str) -> Union[bool,User]:
+    user = asyncio.run(crud.user.get_by_id(db, user_id))
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
