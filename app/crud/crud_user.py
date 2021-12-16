9efc935cbf6235db23415d62977bed8fd679911e
Wellington Lorenço <wlsouza@protonmail.com>
2021-12-16 20:36:30 -0300

    refactor: change imports of models and schemas

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -3,36 +3,35 @@ from typing import Optional, Union, List, Dict, Any, Literal
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models.user import User
-from app.schemas.user import UserCreate, UserUpdate
+from app import models, schemas
 from app.core.security import get_password_hash, verify_password
 
 
 class CrudUser():
 
-    async def get_by_id(self, db:AsyncSession, id:int) -> Optional[User]:
+    async def get_by_id(self, db:AsyncSession, id:int) -> Optional[models.User]:
         result = await db.execute(
-            select(User).where(User.id == id)
+            select(models.User).where(models.User.id == id)
         )
         return result.scalar()
 
     async def get_multi(
         self, db:AsyncSession, skip:int=0, limit:int=100
-    ) -> Optional[List[User]]:
+    ) -> Optional[List[models.User]]:
         result = await db.execute(
-            select(User).offset(skip).limit(limit)
+            select(models.User).offset(skip).limit(limit)
         )
         return result.scalars().all()
         
-    async def get_by_email(self, db:AsyncSession, email:str) -> Optional[User]:
+    async def get_by_email(self, db:AsyncSession, email:str) -> Optional[models.User]:
         result = await db.execute(
-            select(User).where(User.email == email)
+            select(models.User).where(models.User.email == email)
         )
         return result.scalar()
 
     async def create(
-        self, db:AsyncSession, user_in: Union[UserCreate, Dict[str, Any]]
-    ) -> User:
+        self, db:AsyncSession, user_in: Union[schemas.UserCreate, Dict[str, Any]]
+    ) -> models.User:
         if isinstance(user_in, dict):
             user_data = user_in.copy()
         else:
@@ -40,15 +39,15 @@ class CrudUser():
         if user_data.get("password"):
             hashed_password = get_password_hash(user_data.pop("password"))
             user_data["hashed_password"] = hashed_password
-        db_user = User(**user_data)
+        db_user = models.User(**user_data)
         db.add(db_user)
         await db.commit()
         await db.refresh(db_user)
         return db_user
 
     async def update(
-        self, db:AsyncSession, db_user: User, user_in: Union[UserUpdate, Dict[str, Any]]
-    ) -> User:
+        self, db:AsyncSession, db_user: models.User, user_in: Union[schemas.UserUpdate, Dict[str, Any]]
+    ) -> models.User:
         if isinstance(user_in, dict):
             update_data = user_in.copy()
         else:
@@ -63,14 +62,14 @@ class CrudUser():
         await db.refresh(db_user)
         return db_user
 
-    async def delete_by_id(self, db:AsyncSession, id:int) -> Optional[User]:
+    async def delete_by_id(self, db:AsyncSession, id:int) -> Optional[models.User]:
         user = await self.get_by_id(db=db, id=id)
         await db.delete(user)
         await db.commit()
         return user
 
     # TODO: Think if authenticate_user method should be in CrudUser class 
-    async def authenticate_user(self, db:AsyncSession, user_email:str , password:str) -> Union[Literal[False],User]:
+    async def authenticate_user(self, db:AsyncSession, user_email:str , password:str) -> Union[Literal[False],models.User]:
         user = await self.get_by_email(db, user_email)
         if not user:
             return False
