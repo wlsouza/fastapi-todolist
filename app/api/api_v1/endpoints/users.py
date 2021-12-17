7c92d0ead805df9848f9bc581cd1e744314a0553
Wellington Lorenço <wlsouza@protonmail.com>
2021-12-16 21:17:58 -0300

    feat: create a get /me/ endpoint to test JWT

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -1,11 +1,10 @@
-from app.schemas import user
 from typing import Any
 
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import schemas, crud
+from app import schemas, models, crud
 from app.api import deps
 
 router = APIRouter()
@@ -13,7 +12,7 @@ router = APIRouter()
 @router.post("/",response_model=schemas.User, status_code=status.HTTP_201_CREATED, responses={400:{"model":schemas.Message}})
 async def create_user(
     user_in: schemas.UserCreate,
-    db: Session = Depends(deps.get_db)
+    db: AsyncSession = Depends(deps.get_db)
     ) -> Any:
     user = await crud.user.get_by_email(db=db, email=user_in.email)
     if user:
@@ -22,4 +21,10 @@ async def create_user(
             content = "Already exists an user with this email.",
         )
     user = await crud.user.create(db=db, user_in=user_in)
+    return user
+
+@router.get("/me/", response_model=schemas.User, status_code=status.HTTP_200_OK)
+async def get_current_user(
+    user:models.User = Depends(deps.get_token_user)
+)-> Any:
     return user
