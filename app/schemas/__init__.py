c2155025e3b7e13fc5365ba37a65428af518bcb6
Wellington Lorenço <wlsouza@protonmail.com>
2021-12-22 10:19:58 -0300

    add HTTPError schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .message import Message
+from .error import HTTPError
 from .token import Token, TokenPayload
 from .task import Task, TaskCreate, TaskUpdate
 from .user import User, UserCreate, UserUpdate
