ed5a220e63f433f81320a7b9f4b4f5f9884e7c94
Wellington Lorenço <wlsouza@protonmail.com>
2022-01-05 20:35:08 -0300

    feat: add a celery task to send verification email

---
+++ app/celery_worker.py
@@ -0,0 +1,25 @@
+import asyncio
+from fastapi_mail import FastMail, MessageSchema
+
+from app.core.celery_app import celery_app
+from app.core.config import settings
+
+@celery_app.task
+async def send_verification_email(email_to:str, first_name:str, token:str):
+    
+    verification_link = f"https://github.com/wlsouza/fastapi-todolist" #TODO: insert link to frontend page resolve it.
+
+    message = MessageSchema(
+        subject = "FastAPI-TodoList Email Verification",
+        recipients = list(email_to),
+        template_body = {
+            "first_name": first_name,
+            "verification_link": verification_link
+        }
+    )
+
+    fm = FastMail(settings.EMAIL_CONNECTION_CONFIG)
+    asyncio.run( 
+        fm.send_message(message, template_name="verification_email.html")
+    )
+
--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,52 @@
 import secrets
-from typing import Union
+from typing import Union, Optional, Dict, Any
+from pathlib import Path
 
-from pydantic import BaseSettings, PostgresDsn
+from pydantic import BaseSettings, PostgresDsn, validator
+from fastapi_mail import ConnectionConfig
 
 
 class Settings(BaseSettings):
     BASE_URL: str = "http://localhost"
     API_V1_STR: str = "/api/v1"
+
+    # Auth configs
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 2 # 60 min * 24 hrs * 2 days = 2 days
+
+    # DB configs
     SQLALCHEMY_DATABASE_URI: Union[str, PostgresDsn] = "postgresql+asyncpg://postgres:postgres@localhost:5432/FASTAPI_TODOLIST"
+    
+    # Broker and Celery configs
     BROKER_URI: str = "amqp://guest@localhost:5672//" 
 
+    # Email configs
+    MAIL_USERNAME: Optional[str] = None
+    MAIL_PASSWORD: Optional[str] = None
+    MAIL_FROM: Optional[str] = None
+    MAIL_PORT: Optional[int] = None
+    MAIL_SERVER: Optional[str] = None
+    MAIL_TLS: Optional[bool] = None
+    MAIL_SSL: Optional[bool] = None
+    EMAIL_TEMPLATES_DIR: Path = Path("/app/app/email-templates/build")
+    EMAIL_CONNECTION_CONFIG: Optional[ConnectionConfig] = None 
+
+    @validator("EMAIL_CONNECTION_CONFIG", pre=True)
+    def assemble_email_config(cls, v: Optional[ConnectionConfig], values: Dict[str, Any]) -> Any:
+        if isinstance(v, ConnectionConfig):
+            return v
+        return ConnectionConfig(
+            MAIL_USERNAME=values.get("MAIL_USERNAME"),
+            MAIL_PASSWORD=values.get("MAIL_PASSWORD"),
+            MAIL_FROM=values.get("MAIL_FROM"),
+            MAIL_PORT=values.get("MAIL_PORT"),
+            MAIL_SERVER=values.get("MAIL_SERVER"),
+            MAIL_TLS=values.get("MAIL_TLS"),
+            MAIL_SSL=values.get("MAIL_SSL"),
+            TEMPLATE_FOLDER=values.get("EMAIL_TEMPLATES_DIR"),
+        )
+    
 
     class Config:
         env_file = ".env"
